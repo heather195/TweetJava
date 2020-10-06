91a72f471a12b72259c6c6cef05b0df0686333eb
Heather <heather.craddock@hotmail.co.uk>
2020-10-05 23:12:44 -0400

    updated class and tests

--- src/main/java/com/Tweet.java
+++ src/main/java/com/Tweet.java
@@ -1,4 +1,41 @@
 package com;
 
 public class Tweet {
+
+    /*
+    private message:String<br/>
+    public addLetter(c:char):void<br/>
+    public deleteLetter():void<br/>
+
+    Notes:
+    A Tweet has a max character length of 140 characters
+     */
+
+    private String message;
+    private static final int MAX_TWEET_LENGTH = 140;
+
+    public Tweet() {
+        this.message = "";
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void addLetter(char c){
+        if(this.message.length() < MAX_TWEET_LENGTH){
+            setMessage(this.message + c);
+        } else {
+            System.out.println("Tweet too long!");
+        }
+
+    }
+
+    public void deleteLetter(){
+        setMessage((this.message).substring(0, (this.message).length()-1));
+    }
 }
--- src/test/java/com/TweetTest.java
+++ src/test/java/com/TweetTest.java
@@ -5,4 +5,60 @@ import org.junit.Test;
 
 public class TweetTest {
 
+    // test adding a letter
+    @Test
+    public void testAddLetter(){
+        Tweet tweet = new Tweet();
+        tweet.addLetter('a');
+        String expected = "a";
+        Assert.assertEquals(expected, tweet.getMessage());
+    }
+
+    //test adding multiple letters
+    @Test
+    public void testAddMultipleLetters(){
+        Tweet tweet = new Tweet();
+        tweet.addLetter('a');
+        tweet.addLetter('b');
+        tweet.addLetter('c');
+        String expected = "abc";
+        Assert.assertEquals(expected, tweet.getMessage());
+    }
+
+    // test adding and deleting a letter
+    @Test
+    public void testAddDeleteLetters(){
+        Tweet tweet = new Tweet();
+        tweet.addLetter('a');
+        tweet.addLetter('b');
+        tweet.addLetter('c');
+        tweet.deleteLetter();
+        String expected = "ab";
+        Assert.assertEquals(expected, tweet.getMessage());
+    }
+
+    @Test
+    public void testMax140(){
+        Tweet tweet = new Tweet();
+        String testword = "";
+        for(int i = 0; i < 140; i++){
+            testword += "a";
+            tweet.addLetter('a');
+        }
+        tweet.addLetter('b');
+        tweet.addLetter('c');
+        Assert.assertEquals(testword, tweet.getMessage());
+    }
+
+    // test deleting a letter from empty tweet
+    /*
+    @Test
+    public void testDeleteFromEmpty(){
+        Tweet tweet = new Tweet();
+        tweet.deleteLetter();
+    }*/
+
+    // test if it will reject tweet over 140 characters
+
+
 }
